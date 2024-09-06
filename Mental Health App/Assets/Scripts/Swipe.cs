0ecfcdc0e21f7f654b690034cbc763d1bd10fbb7
jSmith-Apollo <chad.smith@stu.daviess.kyschools.us>
2024-09-06 13:14:42 -0500

    SwipeControlsDay3
    Still working on the bugs

--- Mental Health App/Assets/Scripts/Swipe.cs
+++ Mental Health App/Assets/Scripts/Swipe.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[DefaultExecutionOrder(-1)]
 public class Swipe : MonoBehaviour
 {
+    public Sprite ArrowImage;
+    public List<Sprite> Arrows;
+    public int ArrowID;
+
     private Vector2 startPos;
     private Vector2 endPos;
+    private int deadzone = 10;
 
     private void Update()
     {
@@ -22,25 +28,25 @@ public class Swipe : MonoBehaviour
             Debug.Log("EndedTouch");
 
             //Swipe left
-            if (endPos.x < startPos.x)
+            if (endPos.x < startPos.x-deadzone)
             {
                 SLeft();
             }
 
             //Swipe Right
-            if (endPos.x > startPos.x)
+            if (endPos.x > startPos.x+deadzone)
             {
                 SRight();
             }
 
             //Swipe Down
-            if (endPos.y < startPos.y)
+            if (endPos.y > startPos.y+deadzone)
             {
                 SDown();
             }
 
             //Swipe Up
-            if (endPos.y > startPos.y)
+            if (endPos.y < startPos.y-deadzone)
             {
                 SUp();
             }
@@ -49,18 +55,23 @@ public class Swipe : MonoBehaviour
 
     private void SLeft()
     {
+        ArrowID = 0;
+        ArrowImage.texture.Equals(Arrows[ArrowID].texture);
         Debug.Log("Swiped Left");
     }
     private void SRight()
     {
+        ArrowID = 1;
         Debug.Log("Swiped Right");
     }
     private void SDown()
     {
+        ArrowID = 2;
         Debug.Log("Swiped Down");
     }
     private void SUp()
     {
+        ArrowID = 3;
         Debug.Log("Swiped Up");
     }
 }
--- Mental Health App/Assets/Scripts/TouchManager.cs
+++ Mental Health App/Assets/Scripts/TouchManager.cs
@@ -3,15 +3,12 @@ using UnityEngine.InputSystem;
 //using UnityEngine.InputSystem.EnhancedTouch;
 //using static UnityEditor.Timeline.TimelinePlaybackControls;
 
-
+[DefaultExecutionOrder(-1)]
 public class TouchManager : MonoBehaviour
 {
 
-    private int YPosStart = 0;
-    private int XPosStart = 0;
-
-    private int YPosEnd = 0;
-    private int XPosEnd = 0;
+    public Vector2 startLoc;
+    public Vector2 endLoc;
 
     [SerializeField]
     private GameObject player;
@@ -44,27 +41,46 @@ public class TouchManager : MonoBehaviour
         //Debug.Log(value);
 
         Vector2 loc = touchPositionAction.ReadValue<Vector2>();
+        if(Input.touchCount > 0)
+        {
+            startLoc = loc;
+        }
+        else
+        {
+            endLoc = loc;
+        }
+        Debug.Log("X pos: " + (int)loc.x + "  |  Y pos: " + (int)loc.y);
         Vector3 position = Camera.main.ScreenToWorldPoint(loc);
         position.z = player.transform.position.z;
         player.transform.position = position;
-        Debug.Log("X pos: " + getXPos() + "  |  Y pos: " + getYPos());
+
 
     }
 
-    private int getYPos()
+    private int getStartYPos()
     {
-        return 0;
+        return (int)startLoc.y;
     }
 
-    private int getXPos()
+    private int getStartXPos()
     {
-        return 0;
+        return (int)startLoc.x;
+    }
+
+    private int getEndYPos()
+    {
+        return (int)endLoc.y;
+    }
+
+    private int getEndXPos()
+    {
+        return (int)endLoc.x;
     }
 
     private void Update()
     {
-        if(touchPositionAction!=null)
-            Debug.Log("X pos: "+getXPos()+"  |  Y pos: "+getYPos());
+        //if(touchPositionAction!=null)
+            //Debug.Log("X pos: "+getXPos()+"  |  Y pos: "+getYPos());
     }
 }
 
