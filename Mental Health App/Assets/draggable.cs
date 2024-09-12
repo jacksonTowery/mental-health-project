684f5e30b73e71b8b6ff271aea1390cd74584472
StephenBender2024 <jackson.wyatt@stu.daviess.kyschools.us>
2024-09-12 13:36:10 -0500

    Worked Then didn't Work
    I love unity

--- Mental Health App/Assets/draggable.cs
+++ Mental Health App/Assets/draggable.cs
@@ -5,5 +5,26 @@ using UnityEngine.InputSystem.Android.LowLevel;
 
 public class draggable : MonoBehaviour
 {
-    
+    bool drag;
+
+    private void OnMouseDown()
+    {
+        drag = true;
+        print("Mouse Down");
+    }
+
+    private void OnMouseUp()
+    {
+        drag = false;
+        print("Mouse up");
+    }
+
+    private void Update()
+    {
+        if (drag)
+        {
+            Vector2 MousePos = Camera.current.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            transform.position = MousePos;
+        }
+    }
 }
