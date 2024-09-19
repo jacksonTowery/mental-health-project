9b0a6836f1247f25ad76d7d2785a6e95bec6a4e4
jSmith-Apollo <chad.smith@stu.daviess.kyschools.us>
2024-09-19 12:42:26 -0500

    Draggables Backup
    I made this to be used in case rigidbodies break the draggables

--- Mental Health App/Assets/draggable.cs
+++ Mental Health App/Assets/draggable.cs
@@ -30,7 +30,7 @@ public class draggable : MonoBehaviour
             //Vector3 AbsPos = new Vector3(Mathf.Abs(transform.localPosition.x), Mathf.Abs(transform.localPosition.y), Mathf.Abs(transform.localPosition.z))*(-1);
             //transform.localPosition = MousePos - AbsPos;
             //transform.localPosition = AbsPos * (-1);
-            Vector2 MousePos = Camera.current.ScreenToViewportPoint(2 * Input.mousePosition) - transform.localPosition;
+            Vector2 MousePos = Camera.current.ScreenToViewportPoint((float)2.5 * Input.mousePosition) - transform.localPosition;
             MousePos.x = MousePos.x - (float)1.75;
             MousePos.y = MousePos.y - (float)3.75;
 
