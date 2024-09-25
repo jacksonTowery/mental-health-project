34bd76332ba9d124d4d92570a7f69a32c91e47ef
jSmith-Apollo <chad.smith@stu.daviess.kyschools.us>
2024-09-25 13:09:16 -0500

    New fun stuff

--- Mental Health App/Assets/draggable.cs
+++ Mental Health App/Assets/draggable.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem.Android.LowLevel;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class draggable : MonoBehaviour
 {
     bool drag;
+    bool idle;
+    Vector2 mouseLastPos;
 
     private void OnMouseDown()
     {
@@ -21,20 +25,39 @@ public class draggable : MonoBehaviour
         print("Mouse up");
     }
 
+    private bool MouseMoved()
+    {
+        if (Input.mousePosition == (Vector3)mouseLastPos)
+        {
+            print("Mouse moved");
+            return false;
+        }
+        print("Mouse idle");
+        return true;
+    }
+
     private void Update()
     {
+        mouseLastPos = Input.mousePosition;
         if (drag)
         {
+            if (MouseMoved())
+            {
+                print("Mouse moving");
+            }
             //Vector3 MousePos = Camera.current.ScreenToViewportPoint(3*Input.mousePosition);
             //transform.localPosition = MousePos - new Vector3(2,4,0);
             //Vector3 AbsPos = new Vector3(Mathf.Abs(transform.localPosition.x), Mathf.Abs(transform.localPosition.y), Mathf.Abs(transform.localPosition.z))*(-1);
             //transform.localPosition = MousePos - AbsPos;
             //transform.localPosition = AbsPos * (-1);
-            Vector2 MousePos = Camera.current.ScreenToViewportPoint((float)2.5 * Input.mousePosition) - transform.localPosition;
+            Vector2 MousePos = Camera.current.ScreenToViewportPoint(Input.mousePosition);
             MousePos.x = MousePos.x - (float)1.75;
             MousePos.y = MousePos.y - (float)3.75;
 
-            transform.Translate(MousePos);
+            print("X: " + MousePos.x + "  Y: " + MousePos.y);
+            //transform.Translate(MousePos);
+            transform.localPosition = MousePos;
+            mouseLastPos = MousePos;
         }
     }
 }
