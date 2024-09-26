c77480c8ce10608f647b1243860d3f2df8bcbbcf
StephenBender2024 <jackson.wyatt@stu.daviess.kyschools.us>
2024-09-26 12:18:10 -0500

    Added Comments + fixed bugs

--- Mental Health App/Assets/Activities.cs
+++ Mental Health App/Assets/Activities.cs
@@ -5,38 +5,43 @@ using Unity.Notifications.Android;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEditor.SceneManagement;
+using System.Threading.Tasks;
+
 public class Activities : MonoBehaviour
 {
     // Start is called before the first frame update
     void Awake()
     {
-    
+
         PermissionRequest request = new PermissionRequest();
         while (request.Status == PermissionStatus.RequestPending)
         {
-           
+            print("pending");
         }
         if (request.Status == PermissionStatus.Denied)
         {
+            print("denied");
             SceneManager.LoadScene("MHStart");
         }
-        //android notification channel
-        AndroidNotificationChannelGroup channelgroup = new AndroidNotificationChannelGroup()
+        else
         {
-            Id = "Main",
-            Name = "MainNotifications"
-        };
-        AndroidNotificationCenter.RegisterNotificationChannelGroup(channelgroup);
-        AndroidNotificationChannel channel = new AndroidNotificationChannel()
-        {
-            Id = "ChannelID",
-            Name = "Channel",
-            Importance = Importance.Default,
-            Description = "Generic notifications"
-        };
-        AndroidNotificationCenter.RegisterNotificationChannel(channel);
+            //android notification channel
+            AndroidNotificationChannelGroup channelgroup = new AndroidNotificationChannelGroup()
+            {
+                Id = "Main",
+                Name = "MainNotifications"
+            };
+            AndroidNotificationCenter.RegisterNotificationChannelGroup(channelgroup);
+            AndroidNotificationChannel channel = new AndroidNotificationChannel()
+            {
+                Id = "ChannelID",
+                Name = "Channel",
+                Importance = Importance.Default,
+                Description = "Generic notifications"
+            };
+            AndroidNotificationCenter.RegisterNotificationChannel(channel);
+        }
     }
-
     // Update is called once per frame
     void Update()
     {
