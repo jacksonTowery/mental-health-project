81f8cc50b5a5655bb31fdee369e1ed9fd47f130a
StephenBender2024 <jackson.wyatt@stu.daviess.kyschools.us>
2024-09-27 13:17:58 -0500

    Got it work

--- Mental Health App/Assets/Activities.cs
+++ Mental Health App/Assets/Activities.cs
@@ -7,74 +7,45 @@ using UnityEngine.SceneManagement;
 using UnityEditor.SceneManagement;
 using System.Threading.Tasks;
 using Unity.Mathematics;
+using UnityEngine.UI;
 
 public class Activities : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Awake()
+    GameObject Text;
+    private void Start()
     {
-
-        //PermissionRequest request = new PermissionRequest();
-        //while (request.Status == PermissionStatus.RequestPending)
-        //{
-        ///    print("pending");
-       // }
-//{
-       //     print("denied");
-       //   SceneManager.LoadScene("MHStart");
-       // }
-      // else
-      //  {
-            //android notification channel
-            AndroidNotificationChannelGroup channelgroup = new AndroidNotificationChannelGroup()
-            {
-                Id = "Main",
-                Name = "MainNotifications"
-            };
-            AndroidNotificationCenter.RegisterNotificationChannelGroup(channelgroup);
-            AndroidNotificationChannel channel = new AndroidNotificationChannel()
-            {
-                Id = "ChannelID",
-                Name = "Channel",
-                Importance = Importance.Default,
-                Description = "Generic notifications"
-            };
-            AndroidNotificationCenter.RegisterNotificationChannel(channel);
-        }
-   // }
+        Text = GameObject.Find("Activity").transform.GetChild(1).gameObject.transform.GetChild(2).gameObject;
+    }
+    // Start is called before the first frame update
     void Update()
     {
 
     }
-    private void Walk()
+    public void Walk()
     {
-        AndroidNotification notif = new AndroidNotification();
-        notif.Title = "Go on a walk:";
-        notif.Text = "Go outside and start walking";
-        AndroidNotificationCenter.SendNotification(notif, "ChannelID");
-
-        int walktime = UnityEngine.Random.Range(0, 10);
-        AndroidNotification StopNotif = new AndroidNotification();
-        StopNotif.Title = "You Have Finished your walk";
-        StopNotif.FireTime = System.DateTime.Now.AddSeconds(walktime);
-        StopNotif.Text = "You have walked for:" + walktime + " Minutes";
-
-        AndroidNotificationCenter.SendNotification(StopNotif, "ChannelID");
-    }
-    
-    private void Exercise()
-    {
-        AndroidNotification notif = new AndroidNotification();
-        notif.Title = "Go on a Exercise:";
-        notif.Text = "";
-        AndroidNotificationCenter.SendNotification(notif, "ChannelID");
+        Text.transform.GetComponent<Text>().text = "Walk for: " + UnityEngine.Random.Range(10, 59) + " Minutes";
     }
 
-    private void Yoga()
+    public void Exercise()
     {
-        AndroidNotification notif = new AndroidNotification();
-        notif.Title = "Do some Yoga:";
-        notif.Text = "";
-        AndroidNotificationCenter.SendNotification(notif, "ChannelID");
+        string[] exercises = new string[3]
+        {
+            "Sit-ups",
+            "push-Ups",
+            "Squats"
+        };
+        Text.transform.GetComponent<Text>().text = "Do " + UnityEngine.Random.Range(1, 4) + " sets of " + UnityEngine.Random.Range(8, 16) + " " + exercises[(int)UnityEngine.Random.Range(0, 2)];
+
+    }
+
+    public void Yoga()
+    {
+        string[] exercises = new string[3]
+        {
+            "Balasana",
+            "TreePose",
+            "Downward Dog"
+        };
+        Text.transform.GetComponent<Text>().text = "Do " + exercises[(int)UnityEngine.Random.Range(0, 2)] + UnityEngine.Random.Range(5, 10) + " Times " ;
     }
 }
